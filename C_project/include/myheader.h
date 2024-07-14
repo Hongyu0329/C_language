e24cccff22c7504b771878ba82272561c1ed6c69
Why-UESTC <101922497+Hongyu0329@users.noreply.github.com>
2024-07-14 22:48:43 +0800

    changes on mac

---
+++ C_project/include/myheader.h
@@ -0,0 +1,7 @@
+// include/myheader.h
+#ifndef MYHEADER_H
+#define MYHEADER_H
+
+// 头文件内容
+
+#endif // MYHEADER_H
---
+++ C_project/src/main.c
@@ -0,0 +1,7 @@
+// src/main.c
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
